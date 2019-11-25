5d815fdf2cf04df2a4d0b0b75ca31ddc5b653e1c
Jakub Domagala <domagala.jacob@gmail.com>
2019-11-25 23:19:15 +0100

    Use googleimagesdownload as shell command

--- downloadAll.py
+++ downloadAll.py
@@ -1,4 +1,4 @@
-from google_images_download import google_images_download
+import subprocess
 import os
 
 f = open('foodlist.txt', 'r')
@@ -8,8 +8,9 @@ f.close()
 numClass = len(foodList)
 print("{} labels".format(numClass))
 path = os.path.dirname(os.path.realpath(__file__)) + "\\downloads"
-g = google_images_download.googleimagesdownload() 
+
+limit = 100
 
 for i in foodList:
     i = i[:-1]
-    g.download({"output_directory" : path, "keywords" : i, "limit":10})
+    subprocess.call("googleimagesdownload --keywords \"{}\" --limit {}".format(i, limit), shell=True)  
